6299d7e157ee56d61c1bd34117be03b94d32f6e4
TammyXin <only@32mb.cn>
2014-02-28 13:16:44 +0800

    优化后台选项
    加入9个新选项

--- functions.php
+++ functions.php
@@ -1,6 +1,17 @@
 <?php
-
 function themeConfig($form) {
     $topNotice = new Typecho_Widget_Helper_Form_Element_Text('topNotice', NULL, NULL, _t('顶部公告'), _t('这里可以输入一段文字显示顶部公告。（留空为不显示）'));
     $form->addInput($topNotice);
+    $sidebarBlock = new Typecho_Widget_Helper_Form_Element_Checkbox('sidebarBlock', 
+    array('OnlyShowIcon' => _t('小工具标题只显示图标'),
+    'ShowSearchBox' => _t('显示搜索工具'),
+    'ShowRecentPosts' => _t('显示最新文章'),
+    'ShowRecentComments' => _t('显示最近回复'),
+    'ShowTagCloud' => _t('显示标签云'),
+    'ShowCategory' => _t('显示分类'),
+    'ShowArchive' => _t('显示归档'),
+    'ShowOther' => _t('显示其它杂项'),
+    'ShowBlogroll' => _t('显示友情链接')),
+    array('OnlyShowIcon','ShowRecentPosts', 'ShowRecentComments','ShowTagCloud', 'ShowCategory', 'ShowArchive',), _t('边栏选项'));  
+    $form->addInput($sidebarBlock->multiMode());
 }
--- sidebar.php
+++ sidebar.php
@@ -2,26 +2,29 @@
 
 <div id="left-sidebar" class="sidebar">
   <!-- searchform start -->
+  <?php if (empty($this->options->sidebarBlock) || in_array('ShowSearchBox', $this->options->sidebarBlock)): ?>
   <div id="searchbox" class="box">
     <form action="" method="get">
       <input type="text" class="searchfield" name="s" size="24" value="" />
       <input type="submit" name="button" id="searchsubmit" value="Search" class="searchbutton" />
     </form>
   </div>
+  <?php endif; ?>
   <!-- searchform end -->
   <!--[if IE 6]>
-				<div class="widget">
-				<div class="widget-browser">
-				<div class="widget-body">
-					<p class="browser">You are using IE 6 right now, we will work better for you if you upgrade to IE 8 or switch to another browser.</p>
-					<a href="http://www.mozilla.com/en-US/" title="Firefox" rel="external nofollow"><img src="<?php $this->options->themeUrl('img/firefox.png'); ?>" width="64" height="64" /></a>&nbsp;
-					<a href="http://www.google.com/chrome" title="Google Chrome" rel="external nofollow"><img src="<?php $this->options->themeUrl('img/chrome.png'); ?>" width="64" height="64" /></a>&nbsp;
-					<a href="http://www.opera.com/" title="Opera" rel="external nofollow"><img src="<?php $this->options->themeUrl('img/opera.png'); ?>" width="64" height="64" /></a>&nbsp;
-					<a href="http://www.apple.com/safari/download/" title="Apple Safari" rel="external nofollow"><img src="<?php $this->options->themeUrl('img/safari.png'); ?>" width="64" height="64" /></a>
-				</div>
-				</div>
-				</div>
-				<![endif]-->
+        <div class="widget">
+        <div class="widget-browser">
+        <div class="widget-body">
+          <p class="browser">You are using IE 6 right now, we will work better for you if you upgrade to IE 8 or switch to another browser.</p>
+          <a href="http://www.mozilla.com/en-US/" title="Firefox" rel="external nofollow"><img src="<?php $this->options->themeUrl('img/firefox.png'); ?>" width="64" height="64" /></a>&nbsp;
+          <a href="http://www.google.com/chrome" title="Google Chrome" rel="external nofollow"><img src="<?php $this->options->themeUrl('img/chrome.png'); ?>" width="64" height="64" /></a>&nbsp;
+          <a href="http://www.opera.com/" title="Opera" rel="external nofollow"><img src="<?php $this->options->themeUrl('img/opera.png'); ?>" width="64" height="64" /></a>&nbsp;
+          <a href="http://www.apple.com/safari/download/" title="Apple Safari" rel="external nofollow"><img src="<?php $this->options->themeUrl('img/safari.png'); ?>" width="64" height="64" /></a>
+        </div>
+        </div>
+        </div>
+        <![endif]-->
+  <?php if (empty($this->options->sidebarBlock) || in_array('ShowRecentPosts', $this->options->sidebarBlock)): ?>
   <div class="widget">
     <h3 class="widget-title"><i class="icon-list"></i>Recent Posts</h3>
   <div class="widget-body recent-posts">
@@ -30,12 +33,14 @@
     </ul>
   </div>
   </div>
+  <?php endif; ?>
+  <?php if (empty($this->options->sidebarBlock) || in_array('ShowRecentComments', $this->options->sidebarBlock)): ?>
   <?php if($this->is('index')): ?>
   <div class="widget">
     <h3 class="widget-title"><i class="icon-user"></i>Recent Comments</h3>
   <div class="widget-body">
     <ul>
-	        <?php $this->widget('Widget_Comments_Recent','ignoreAuthor=true')->to($comments); ?>
+          <?php $this->widget('Widget_Comments_Recent','ignoreAuthor=true')->to($comments); ?>
             <?php while($comments->next()): ?>
                 <li><a href="<?php $comments->permalink(); ?>"><?php
 $u = Helper::options()->siteUrl;
@@ -57,19 +62,22 @@ if ( filesize($e) < 500 ) copy($d, $e);
   </div>
   </div>
   <?php endif; ?>
-
+  <?php endif; ?>
+  <?php if (empty($this->options->sidebarBlock) || in_array('ShowTagCloud', $this->options->sidebarBlock)): ?>
   <div class="widget" id="tagCloud">
   
     <h3 class="widget-title"><i class="icon-tag"></i>Tag Cloud</h3>
     <div class="widget-body">
-	<?php $this->widget('Widget_Metas_Tag_Cloud', 'ignoreZeroCount=1&limit=50')->to($tags); ?>
-	<?php while($tags->next()): ?>
+  <?php $this->widget('Widget_Metas_Tag_Cloud', 'ignoreZeroCount=1&limit=50')->to($tags); ?>
+  <?php while($tags->next()): ?>
     <a href="<?php $tags->permalink(); ?>" title='<?php $tags->name(); ?>'><?php $tags->name(); ?></a> 
-	<?php endwhile; ?>
-	</div>
+  <?php endwhile; ?>
+  </div>
     </div>
+<?php endif; ?>
 </div>
 <div id="right-sidebar" class="sidebar">
+  <?php if (empty($this->options->sidebarBlock) || in_array('ShowCategory', $this->options->sidebarBlock)): ?>
   <div class="widget">
     <h3 class="widget-title"><i class="icon-layout"></i><?php _e('Categories'); ?></h3>
     <div class="widget-body">
@@ -79,38 +87,20 @@ if ( filesize($e) < 500 ) copy($d, $e);
     </ul>
     </div>
   </div>
-
+  <?php endif; ?>
+<?php if (empty($this->options->sidebarBlock) || in_array('ShowBlogroll', $this->options->sidebarBlock)): ?>
 <?php if($this->is('index')): ?>
   <div class="widget">
     <h3 class="widget-title"><i class="icon-users"></i>Friends</h3>
     <div class="widget-body">
     <ul>
-	<?php Links_Plugin::output(); ?>
+  <?php Links_Plugin::output(); ?>
     </ul>
     </div>
   </div>
 <?php endif; ?>
-
-<?php if($this->is('index')): ?>
-  <div class="widget">
-    <h3 class="widget-title"><i class="icon-link"></i>Designs</h3>
-  <div class="widget-body">
-    <ul>
-      <li><a href="http://www.cssbasics.com/" target="_blank">CSS Basics</a></li>
-      <li><a href="http://www.smashingmagazine.com/" target="_blank">Smashing Magazine</a></li>
-      <li><a href="http://maxvoltar.com/" target="_blank">Maxvoltar</a></li>
-      <li><a href="http://www.webdesignerwall.com/" target="_blank">Web Designer Wall</a></li>
-      <li><a href="http://abduzeedo.com/" target="_blank">Abduzeedo</a></li>
-      <li><a href="http://vandelaydesign.com/blog/" target="_blank">Vandelay Design Blog</a></li>
-      <li><a href="http://www.tutorial9.net/" target="_blank">Tutorial9</a></li>
-      <li><a href="http://www.noupe.com/" target="_blank">Noupe</a></li>
-      <li><a href="http://net.tutsplus.com/" target="_blank">Nettuts+</a></li>
-      <li><a href="http://10steps.sg/" target="_blank">10Steps.SG</a></li>
-    </ul>
-  </div>
-  </div>
-  <?php endif; ?>
-
+<?php endif; ?>
+<?php if (empty($this->options->sidebarBlock) || in_array('ShowArchive', $this->options->sidebarBlock)): ?>
   <div class="widget">
     <h3 class="widget-title"><i class="icon-calendar"></i>Archives</h3>
     <div class="widget-body">
@@ -120,12 +110,14 @@ if ( filesize($e) < 500 ) copy($d, $e);
     </ul>
     </div>
   </div>
+<?php endif; ?>
+<?php if (empty($this->options->sidebarBlock) || in_array('ShowOther', $this->options->sidebarBlock)): ?>
   <div class="widget">
     <h3 class="widget-title"><i class="icon-cog"></i>Other</h3>
     <div class="widget-body">
     <ul>
                 <?php if($this->user->hasLogin()): ?>
-					<li class="last"><a href="<?php $this->options->adminUrl(); ?>"><?php _e('进入后台'); ?> (<?php $this->user->screenName(); ?>)</a></li>
+          <li class="last"><a href="<?php $this->options->adminUrl(); ?>"><?php _e('进入后台'); ?> (<?php $this->user->screenName(); ?>)</a></li>
                     <li><a href="<?php $this->options->logoutUrl(); ?>"><?php _e('退出'); ?></a></li>
                 <?php else: ?>
                     <li class="last"><a href="<?php $this->options->adminUrl('login.php'); ?>"><?php _e('登录'); ?></a></li>
@@ -134,5 +126,6 @@ if ( filesize($e) < 500 ) copy($d, $e);
     </ul>
   </div>
   </div>
+<?php endif; ?>
 </div>
 <!-- sidebar end -->
