a5dd8820dd4d0f0d3c530b4b7a456fa41a40171d
TammyXin <only@32mb.cn>
2014-02-28 13:37:33 +0800

    优化无用选项

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ function themeConfig($form) {
     $topNotice = new Typecho_Widget_Helper_Form_Element_Text('topNotice', NULL, NULL, _t('顶部公告'), _t('这里可以输入一段文字显示顶部公告。（留空为不显示）'));
     $form->addInput($topNotice);
     $sidebarBlock = new Typecho_Widget_Helper_Form_Element_Checkbox('sidebarBlock', 
-    array('OnlyShowIcon' => _t('小工具标题只显示图标'),
+    array(
     'ShowSearchBox' => _t('显示搜索工具'),
     'ShowRecentPosts' => _t('显示最新文章'),
     'ShowRecentComments' => _t('显示最近回复'),
@@ -12,6 +12,6 @@ function themeConfig($form) {
     'ShowArchive' => _t('显示归档'),
     'ShowOther' => _t('显示其它杂项'),
     'ShowBlogroll' => _t('显示友情链接')),
-    array('OnlyShowIcon','ShowRecentPosts', 'ShowRecentComments','ShowTagCloud', 'ShowCategory', 'ShowArchive',), _t('边栏选项'));  
+    array('ShowRecentPosts', 'ShowRecentComments','ShowTagCloud', 'ShowCategory', 'ShowArchive',), _t('边栏选项'));  
     $form->addInput($sidebarBlock->multiMode());
 }
