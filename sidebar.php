e8f6a6bc840f97c4f26b743a8274fbda41db3415
Ryan Lieu <github-benzBrake@woai.ru>
2025-12-19 00:21:22 +0800

    fix: 修复侧边栏头像

--- sidebar.php
+++ sidebar.php
@@ -42,25 +42,10 @@
                     <ul>
                         <?php $this->widget('Widget_Comments_Recent', 'ignoreAuthor=true')->to($comments); ?>
                         <?php while ($comments->next()): ?>
-                            <li><a href="<?php $comments->permalink(); ?>">
-                                    <?php
-                                    $u = Helper::options()->siteUrl;
-                                    $p = 'usr/img/avatar/';
-                                    $d = $u . $p . 'default.jpg';
-                                    $t = 1209600; // 設定: 14 天, 單位: 秒
-                                    $f = md5(strtolower($comments->mail));
-                                    $a = $u . $p . $f . '.jpg';
-                                    $e = __TYPECHO_ROOT_DIR__ . '/' . $p . $f . '.jpg';
-                                    if (!is_file($e) || (time() - filemtime($e)) > $t) {//當頭像不存在或文件超過14天才更新
-                                        $g = 'http://www.gravatar.com/avatar/' . $f . '?s=32&d=' . $d . '&r=X';
-                                        copy($g, $e);
-                                        $a = htmlspecialchars($g);
-                                    }
-                                    if (filesize($e) < 500) copy($d, $e);
-                                    ?>
-                                    <img class="avatar" src="<?php echo $a ?>" alt=""
-                                         title="<?php echo $comments->author; ?>"/><?php $comments->author(false); ?>
-                                    ：</br><?php $comments->excerpt(10, '...'); ?></a></li>
+                            <li>
+                                <a href="<?php $comments->permalink(); ?>">
+                                    <?php $comments->gravatar(); ?>
+                                    <?php $comments->author(false); ?>：</br><?php $comments->excerpt(10, '...'); ?></a></li>
                         <?php endwhile; ?>
                     </ul>
                 </div>
