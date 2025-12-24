29a0ad75ecc2714aff618eeb72b8b4615ab868cb
Ryan Lieu <github-benzBrake@woai.ru>
2025-12-24 20:18:07 +0800

    fix: 继续修复 IE6 下的排版 BUG

--- assets/js/base.js
+++ assets/js/base.js
@@ -83,6 +83,11 @@ jQuery.fx.ScrollTo = function (e, o) {
 };
 (function ($) {
 	$(document).ready(function () {
+		var isLtIE7 = (navigator.userAgent.indexOf("MSIE") != -1 && parseFloat(navigator.appVersion.split("MSIE")[1]) < 7);
+		if (isLtIE7) {
+			updateIcon();
+		}
+
 		$("a[rel='external'],a[rel='external nofollow']").click(function () { window.open(this.href); return false });
 		$('.gotop').click(function () { try { $('#header').ScrollTo(800); } catch (e) { } return false; });
 		$('.addcomment').click(function () { try { $('#respond').ScrollTo(1000); } catch (e) { } return false; });
@@ -139,5 +144,70 @@ jQuery.fx.ScrollTo = function (e, o) {
 		});
 
 	});
+
+	function updateIcon () {
+
+		var icons = {
+			'icon-arrow-right': '&#xe61b;',
+			'icon-arrow-left': '&#xe61c;',
+			'icon-arrow-down': '&#xe606;',
+			'icon-arrow-up': '&#xe61f;',
+			'icon-pencil': '&#xe61d;',
+			'icon-paperclip': '&#xe620;',
+			'icon-users': '&#xe621;',
+			'icon-user': '&#xe622;',
+			'icon-star1': '&#xe623;',
+			'icon-star2': '&#xe624;',
+			'icon-thumbs-up': '&#xe625;',
+			'icon-thumbs-down': '&#xe626;',
+			'icon-chat': '&#xe627;',
+			'icon-comment': '&#xe628;',
+			'icon-quote': '&#xe629;',
+			'icon-share': '&#xe62a;',
+			'icon-tag': '&#xe62b;',
+			'icon-calendar': '&#xe62c;',
+			'icon-googleplus': '&#xe62d;',
+			'icon-tumblr': '&#xe62e;',
+			'icon-facebook': '&#xe62f;',
+			'icon-list': '&#xe630;',
+			'icon-layout': '&#xe61e;',
+			'icon-text': '&#xe631;',
+			'icon-rss': '&#xe632;',
+			'icon-checkmark': '&#xe633;',
+			'icon-lock': '&#xe634;',
+			'icon-export': '&#xe635;',
+			'icon-heart2': '&#xe636;',
+			'icon-heart3': '&#xe637;',
+			'icon-forward': '&#xe638;',
+			'icon-reply': '&#xe639;',
+			'icon-earth': '&#xe63a;',
+			'icon-info': '&#xe63b;',
+			'icon-warning': '&#xe63c;',
+			'icon-shuffle': '&#xe63d;',
+			'icon-resize-enlarge': '&#xe63e;',
+			'icon-resize-shrink': '&#xe63f;',
+			'icon-sina-weibo': '&#xe640;',
+			'icon-renren': '&#xe641;',
+			'icon-qq': '&#xe642;',
+			'icon-pinterest': '&#xe643;',
+			'icon-linkedin': '&#xe644;',
+			'icon-paypal': '&#xe645;',
+			'icon-picasa': '&#xe646;',
+			'icon-mixi': '&#xe647;'
+		};
+
+		$.each(icons, function (key, value) {
+			var icon = $('.' + key);
+			icon.html(value);
+			icon.css('font-family', 'SimpX');
+			icon.css('font-style', 'normal');
+			icon.css('font-weight', 'normal');
+			icon.css('font-variant', 'normal');
+			icon.css('text-transform', 'none');
+			icon.css('line-height', 1);
+			icon.css('speak', 'none');
+		});
+	}
+
 
 })(jQuery);
